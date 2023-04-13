28c531f93a8f2dd099088eef8088d08333b4ed8f
jjiiaarr <jiarr.glorioso@gmail.com>
2023-04-13 20:27:23 +0800

    add authentication for email at user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :authentication_keys => [:username]
+         :recoverable, :rememberable, :validatable, :authentication_keys => [:username, :email]
 end
