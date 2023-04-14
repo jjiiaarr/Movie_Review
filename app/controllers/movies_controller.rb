f32babeff6cb93cfce99ca74acde3a8abc144e29
jjiiaarr <jiarr.glorioso@gmail.com>
2023-04-14 10:35:54 +0800

    finish Movies Controller CRUD

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,9 +8,37 @@ class MoviesController < ApplicationController
   end
 
   def create
-    movie = Movie.new(params[:movie].permit(:title, :blurb, :date_released, :country, :showing_start, :showing_end))
-    if movie.save
+    @movie = Movie.new(params[:movie].permit(:title, :blurb, :date_released, :country, :showing_start, :showing_end))
+    if @movie.save
       redirect_to movies_path
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(params.require(:movie).permit(:title, :blurb, :date_released, :country, :showing_start, :showing_end))
+      flash[:notice] = 'Movie updated successfully'
+      redirect_to movies_path
+    else
+      flash.now[:alert] = 'Movie update failed'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @movies = Movie.find(params[:id])
+    @movies.destroy
+    flash[:notice] = 'Movie deleted successfully'
+    redirect_to movies_path
   end
 end
