0be16f6a55e555a5587763f47fd2d2eb193e81ca
jjiiaarr <jiarr.glorioso@gmail.com>
2023-04-18 18:17:53 +0800

    done generate and import location controller

---
+++ app/javascript/controllers/location_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="location"
+export default class extends Controller {
+
+  static targets = ['selectedRegionId', 'selectProvinceId']
+
+  fetchProvinces(){
+    let target = this.selectProvinceIdTarget
+    $(target).empty();
+
+    $.ajax({
+      type: 'GET',
+      url: '/api/v1/regions/' + this.selectedRegionIdTarget.value + '/provinces',
+      dataType: 'json',
+      success: (response) => {
+        console.log(response)
+        $.each(response, function (index, record) {
+          let option = document.createElement('option')
+          option.value = record.id
+          option.text = record.name
+          target.appendChild(option)
+        })
+      }
+    })
+  }
+
+}
