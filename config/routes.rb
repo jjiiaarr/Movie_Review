c3ab2ddea0c8098f93a862d1412d0e5321cdb30e
jjiiaarr <jiarr.glorioso@gmail.com>
2023-04-18 16:37:47 +0800

    done api v1

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,22 @@ Rails.application.routes.draw do
   resources :movies do
     resources :reviews, except: :show
   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :regions, only: %i[index show], defaults: { format: :json } do
+        resources :provinces, only: :index, defaults: { format: :json }
+      end
+
+      resources :provinces, only: %i[index show], defaults: { format: :json } do
+        resources :cities, only: :index, defaults: { format: :json }
+      end
+
+      resources :cities, only: %i[index show], defaults: { format: :json } do
+        resources :barangays, only: :index, defaults: { format: :json }
+      end
+
+      resources :barangays, only: %i[index show], defaults: { format: :json }
+    end
+  end
 end
