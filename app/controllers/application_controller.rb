0aef9da0c5f8eff58f76be69ad205780fc1c8333
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-04-19 23:30:03 +0800

    done config permit parameters for sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation, :address_region_id, :address_province_id, :address_city_id, :address_barangay_id, :address])
+  end
 end
